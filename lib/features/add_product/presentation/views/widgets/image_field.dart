a1663c68adfcef02b911c95a1889f7989219f5bd
tasneemakhaled <khaledtasneem402@gmail.com>
2026-04-29 19:36:50 +0300

    image picker done

--- lib/features/add_product/presentation/views/widgets/image_field.dart
+++ lib/features/add_product/presentation/views/widgets/image_field.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class ImageField extends StatelessWidget {
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(16),
+    return Skeletonizer(
+      enabled: isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          isLoading = true;
+          setState(() {});
+          try {
+            final picker = ImagePicker();
+            // Pick an image.
+            final XFile? image = await picker.pickImage(
+              source: ImageSource.gallery,
+            );
+          } on Exception catch (e) {
+            // TODO
+          }
+          isLoading = false;
+          setState(() {});
+        },
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Icon(Icons.image_outlined, size: 150),
+        ),
       ),
-      child: Icon(Icons.image_outlined, size: 150),
     );
   }
 }
