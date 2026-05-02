210fc3c2704042ae9b10c0f36b2d144a3bc32b7d
tasneemakhaled <khaledtasneem402@gmail.com>
2026-05-02 22:26:15 +0300

    create storage service

---
+++ lib/core/services/fire_storage.dart
@@ -0,0 +1,9 @@
+import 'package:fruits_hub_dashboard/core/services/storage_service.dart';
+
+class FireStorage implements StorageService {
+  @override
+  Future<String> uploadFile(String file) {
+    // TODO: implement uploadFile
+    throw UnimplementedError();
+  }
+}
